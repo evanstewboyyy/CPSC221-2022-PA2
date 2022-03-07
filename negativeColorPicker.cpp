9041537a5366351ff3e75edfd4be536bd1d297d4
evanstewboyyy <70913772+evanstewboyyy@users.noreply.github.com>
2022-03-06 18:58:37 -0800

    Update negativeColorPicker.cpp

--- negativeColorPicker.cpp
+++ negativeColorPicker.cpp
@@ -9,12 +9,30 @@
 NegativeColorPicker::NegativeColorPicker(PNG& inputimg)
 {
   // complete your implementation below
-  
+  img = inputimg;
 }
 
 HSLAPixel NegativeColorPicker::operator()(PixelPoint p)
 {
   // complete your implementation below
-  
-  return HSLAPixel(); // REPLACE THIS STUB
+  HSLAPixel *color = img.getPixel(p.x, p.y);
+  HSLAPixel ret;
+  if(color->h < 180)
+  {
+    ret.h = color->h + 180;
+    ret.s = color->s;
+    ret.l = 1 - color->l;
+    ret.a = color->a;
+    return ret;
+    // return *(new HSLAPixel(180 + color.h, color.s, 1 - color.l, color.a));
+  }
+  else
+  {
+    ret.h = color->h - 180;
+    ret.s = color->s;
+    ret.l = 1 - color->l;
+    ret.a = color->a;
+    return ret;
+    // return *(new HSLAPixel(color.h - 180, color.s, 1 - color.l, color.a));
+  }
 }
